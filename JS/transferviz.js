57e909a01715342506efd90858ee076e6b0a9a80
Jurek Müller <jurek_mueller@yahoo.de>
2023-08-18 15:09:36 +0200

    Added player view

--- JS/transferviz.js
+++ JS/transferviz.js
@@ -7,11 +7,12 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Define global variables
-let transferData;
+let playerData;
 let clubData;
 let seasonsData;
-let markerGroup = L.featureGroup();
-let ybCoordinates;
+let markerGroup = L.featureGroup().addTo(map);
+let selectedSeason;
+let selectedView;
 
 // Load the Data from multiple files
 Promise.all([
@@ -19,25 +20,71 @@ Promise.all([
     d3.json('../Data/clubs.geojson'),
     d3.json('../Data/seasons.json')
 ]).then(function([tData, cData, sData]) {
-    transferData = tData;
+    playerData = tData;
     clubData = cData;
     seasonsData = sData;
-    let selectedSeason = seasonsData[seasonsData.length-2].season;
-    let selectedView = "left"; // left, joined, player
-    
-    // Get coordinates for club 452
-    clubData.features.forEach(function(clubFeature) { if(clubFeature.properties.id === "452") {
-        ybCoordinates = clubFeature.geometry.coordinates;
-    }});
+    selectedSeason = seasonsData[seasonsData.length-2].season;
+    selectedView = "left"; // left, joined, playerID
+
+    // Reverse the coordinates for each feature in clubData because leaflet uses [lat,lon] instead of [lon,lat] in geojson
+    clubData.features.forEach(feature => {
+        if (feature.geometry && feature.geometry.coordinates) {
+            feature.geometry.coordinates.reverse();
+        }
+    });
     
     updateMap(selectedSeason, selectedView);
-    // // Add the data to the map
-    // L.geoJSON(clubData, {
-    //     pointToLayer: createIconMarker
-    // }).addTo(map);
 }).catch(function(error) {
     console.log(error);
 });
+
+// Define a function to update the map based on the selected season and view
+function updateMap(selectedSeason, selectedView) {
+    // Clear existing markers from the markerGroup
+    markerGroup.clearLayers();
+
+    const seasonData = seasonsData.filter(d => d.season == selectedSeason)[0];
+    let transferList = [];  // to store the clubId along with the player details
+
+    if (selectedView === "left" || selectedView === "joined") {
+        const players = seasonData["players_"+selectedView]
+        for (let player of players) {
+            const transfer = player.player_transfer_history.find(d => d.season == selectedSeason && d["team_"+selectedView+"_TM_id"] === "452");
+            transferList.push({transferDetail: transfer, player: player});
+        }
+    } else {
+        // In this case selectedView is the player name
+        const player = playerData.find(d => d.player_name == selectedView);
+        for (let transfer of player.player_transfer_history) {
+            // only show transfer arrow if club is in the clubData
+            if (clubData.features.find(feature => feature.properties.id === transfer.team_left_TM_id) && 
+                        clubData.features.find(feature => feature.properties.id === transfer.team_joined_TM_id)) {
+                transferList.push({transferDetail: transfer, player: player});
+            }
+        }
+    }
+    // Create a set of clubIds from transferDetails
+    let clubIdsSet = new Set();
+    transferList.forEach(transfer => {
+        clubIdsSet.add(transfer.transferDetail.team_left_TM_id);
+        clubIdsSet.add(transfer.transferDetail.team_joined_TM_id);
+    });
+    // Add markers for clubs
+    // Filter the clubData based on the extracted club ids and add each to the markerGroup
+    clubData.features.forEach(function(clubFeature) {
+        if (clubIdsSet.has(clubFeature.properties.id)) {
+            createIconMarker(clubFeature, clubFeature.geometry.coordinates);
+        }
+    });
+
+    // Create arrows with player details
+    for (let transfer of transferList) {
+        const clubFeatureTo = clubData.features.find(feature => feature.properties.id === transfer.transferDetail.team_joined_TM_id);
+        const clubFeatureFrom = clubData.features.find(feature => feature.properties.id === transfer.transferDetail.team_left_TM_id);
+        createTransferArrow(clubFeatureFrom.geometry.coordinates, clubFeatureTo.geometry.coordinates, transfer.player);
+    }
+
+}
 
 // Define a function to create a custom icon marker for a club
 function createIconMarker(feature, latlng) {
@@ -56,72 +103,25 @@ function createIconMarker(feature, latlng) {
         //popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
-    return L.marker(latlng, {icon: clubIcon, zIndexOffset: players});
-}
-
-function updateMap(selectedSeason, selectedView) {
-    // Clear existing markers from the markerGroup
-    markerGroup.clearLayers();
-
-    if (selectedView == "left") {
-        let players = seasonsData.filter(function(d) { return d.season == selectedSeason; })[0].players_left;
-
-        // Extract the club ids
-        let clubIds = players.map(function(d) {
-            return d.player_transfer_history.filter(function(d) {
-                return d.season == selectedSeason && d.team_left_TM_id === "452";
-            })[0].team_joined_TM_id;
+    L.marker(latlng, {icon: clubIcon, zIndexOffset: players})
+        .addTo(markerGroup)
+        .bindPopup("Club Info: " + feature.properties.name)  // Bind a popup
+        .on('mouseover', function() {
+            // Open the popup when marker is hovered
+            this.openPopup();
+        })
+        .on('mouseout', function() {
+            // Close the popup when mouse is moved away
+            this.closePopup();
+        })
+        .on('click', function() {
+            // Handle click event here
+            
         });
-        clubIds.push("452");
-
-        // Add the club markers
-        addClubMarker(clubIds);
-
-        // Now, create arrows from 452 to other markers
-        clubData.features.forEach(function(clubFeature) {
-            if (clubIds.includes(clubFeature.properties.id) && clubFeature.properties.id !== "452") {
-                createArrow(ybCoordinates, clubFeature.geometry.coordinates);
-            }
-        });
-    } else if (selectedView == "joined") {
-        let players = seasonsData.filter(function(d) { return d.season == selectedSeason; })[0].players_joined;
-        // Extract the club ids
-        let clubIds = players.map(function(d) {
-            return d.player_transfer_history.filter(function(d) {
-                return d.season == selectedSeason && d.team_joined_TM_id === "452";
-            })[0].team_left_TM_id;
-        });
-        clubIds.push("452");
-
-        // Add the club markers
-        addClubMarker(clubIds);
-
-        // Now, create arrows from 452 to other markers
-        clubData.features.forEach(function(clubFeature) {
-            if (clubIds.includes(clubFeature.properties.id) && clubFeature.properties.id !== "452") {
-                createArrow(clubFeature.geometry.coordinates, ybCoordinates);
-            }
-    });
 }
 
-    // Add markerGroup to the map if it's not already added
-    if (!map.hasLayer(markerGroup)) {
-        markerGroup.addTo(map);
-    }
-}
-
-function addClubMarker(clubIds) {
-    // Filter the clubData based on the extracted club ids and add each to the markerGroup
-    clubData.features.forEach(function(clubFeature) {
-        if (clubIds.includes(clubFeature.properties.id)) {
-            let marker = createIconMarker(clubFeature, clubFeature.geometry.coordinates.reverse());
-            markerGroup.addLayer(marker);
-        }
-    });
-}
-
-// This function creates an arrow (line) between two sets of coordinates
-function createArrow(fromCoords, toCoords) {
+// // This function creates an arrow (line) between two sets of coordinates
+function createTransferArrow(fromCoords, toCoords, player) {
     // Define the path options (color, weight etc.) based on your requirements
     let pathOptions = {
         color: 'black',
@@ -134,10 +134,49 @@ function createArrow(fromCoords, toCoords) {
     markerGroup.addLayer(arrow);
     let arrowHead = L.polylineDecorator(arrow, {
         patterns: [{
-            offset: '50%', // Place arrow at the end of the line
-            repeat: 0,
+            offset: '25%', // Place arrow at the end of the line
+            repeat: '50%',
             symbol: L.Symbol.arrowHead({pixelSize: 10, polygon: false, pathOptions: pathOptions})
         }]
     });
     markerGroup.addLayer(arrowHead);
+    
+    // Calculate midpoint coordinates
+    let midPoint = getMidPoint(fromCoords, toCoords);
+
+    // Create a custom icon using the player's image link
+    iconSize=[32,32]
+    let playerIcon = L.icon({
+        iconUrl: player.player_image_link,
+        iconSize: iconSize,
+        iconAnchor: [iconSize[0] / 2, iconSize[1] / 2]
+    });
+
+    // Place the marker at the midpoint with the custom icon
+    let playerMarker = L.marker(midPoint, { icon: playerIcon })
+        .addTo(markerGroup)
+        .bindPopup("Player Info: " + player.player_name)  // Bind a popup
+        .on('mouseover', function() {
+            // Open the popup when marker is hovered
+            this.openPopup();
+        })
+        .on('mouseout', function() {
+            // Close the popup when mouse is moved away
+            this.closePopup();
+        })
+        .on('click', function() {
+            // Handle click event here
+            selectedView = player.player_name;
+            updateMap(selectedSeason, selectedView);
+        });
+}
+
+function getMidPoint(start, end) {
+    // Convert lat-lon to pixel coordinates
+    const p1 = map.project(start);
+    const p2 = map.project(end);
+    // Calculate pixel midpoint
+    const pixelMidPoint = p1._add(p2)._divideBy(2);
+    // Convert pixel midpoint back to lat-lon
+    return map.unproject(pixelMidPoint);
 }
